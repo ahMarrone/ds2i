5f78c5a738d3779f8a9f79297d1da75eead022b7
ahMarrone <agustinhmarrone@gmail.com>
2018-08-06 13:36:36 -0300

    Update resume_query_logs

--- scripts/resume_query_logs.py
+++ scripts/resume_query_logs.py
@@ -12,8 +12,8 @@ query_log_file_path = sys.argv[3]
 
 ########
 
-QUERY_TYPES = ["or_freq", "maxscore", "maxscore_dyn"]
-N_QUERIES = 500
+QUERY_TYPES = ["maxscore", "maxscore_dyn", "maxscore_smart_dyn"]
+N_QUERIES = int(sys.argv[4])
 
 ## lexicon 
 lexicon_data = []
@@ -44,7 +44,7 @@ query_proccessed_number = 0
 tmp_list = []
 for query_time in query_log_file:
 	if (query_proccessed_number < N_QUERIES):
-		tmp_list.append(int(query_time))
+		tmp_list.append(float(query_time))
 		query_proccessed_number += 1
 	else:
 		query_type_index += 1
@@ -54,23 +54,26 @@ for query_time in query_log_file:
 		continue
 
 # Ya tengo las tres listas con los tiempos de cada estrategia
-print "Q_DATA \t" + "\t".join(QUERY_TYPES) + "\t" + "MIN\t MAX"
+print "Q_DATA \t" + "\t".join(QUERY_TYPES) + "\t" + "DELTAS \t MIN\t MAX"
 min_counts = [0,0,0]
 max_counts = [0,0,0]
 for k in range(len(query_metadata[0])):
 	values = [l[k] for l in query_metadata]
 	minpos = values.index(min(values))
 	maxpos = values.index(max(values))
+	#if maxpos != 2:
+	#	import pdb; pdb.set_trace()
 	min_counts[minpos] += 1
 	max_counts[maxpos] += 1
 	term_id = int(queries_list[k][0])
 	postings_lengths = [lexicon_data[int(entry)][1] for entry in queries_list[k]]
+	time_deltas = [v - values[0] for v in values[1:]]
 	#print term_id
 	#import pdb; pdb.set_trace()
 	#print lexicon_data[int(queries_list[k][0])][1]
 	#print lexicon_data[0]
 	q_data = str(len(queries_list[k])) + ":" + ";".join(postings_lengths)
-	print  q_data + "\t" + str(values[0]) + "\t" + str(values[1]) + "\t" + str(values[2]) + "\t" + str(minpos) + "\t" + str(maxpos)
+	print  q_data + "\t" + str(values[0]) + "\t" + str(values[1]) + "\t" + str(values[2]) + "\t" + ";".join(str(x) for x in time_deltas) + "\t" + str(minpos) + "\t" + str(maxpos)
 print "MIN counts -> " + '\t'.join(map(str, min_counts))
 print "MAX counts -> " + '\t'.join(map(str, max_counts))
 
